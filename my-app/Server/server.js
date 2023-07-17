150167ff3ff277fb60cddf30c4fc7e4618249420
SaiKiran474 <130563472+SaiKiran474@users.noreply.github.com>
2023-07-17 14:44:23 +0530

    Update server.js

--- my-app/Server/server.js
+++ my-app/Server/server.js
@@ -14,21 +14,22 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 app.use((req, res, next) => {
-  const allowedOrigins = ['https://blockchainscm.netlify.app', 'http://localhost:3000'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  }
+  // const allowedOrigins = ['https://blockchainscm.netlify.app', 'http://localhost:3000'];
+  // const origin = req.headers.origin;
+  // if (allowedOrigins.includes(origin)) {
+  //   res.setHeader('Access-Control-Allow-Origin', origin);
+  //   res.setHeader('Access-Control-Allow-Credentials', 'true');
+  // }
   // Rest of your CORS middleware configuration
 
   // Handle preflight OPTIONS request
-  if (req.method === 'OPTIONS') {
+  // if (req.method === 'OPTIONS') {
+  res.setHeader('Access-Control-Allow-Origin', 'https://blockchainscm.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.sendStatus(200);
-    return;
-  }
+  //   return;
+  // }
 
   next();
 });
