75a790313413469f6a9391b64ccbfdb9413d7c12
SaiKiran474 <saikiranchowdary474@gmail.com>
2023-07-17 11:14:06 +0530

    react application

---
+++ my-app/react/src/components/Login.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import "../index.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+<<<<<<< HEAD
+import { toast,ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function Log() {
+  const navigate = useNavigate();
+  const [values, setvalues] = useState({ Id: "", Password: "" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.get(`http://localhost:4000/Login?param1=${values.Id}&param2=${values.Password}`, {
+        withCredentials: true,
+      });
+      console.log(data);
+      if (data) {
+        if (data==="Invalid Password" || data==="Invalid User Id") {
+        //   const { email, password } = data.errors;
+          if (data==="Invalid User Id") showError("Invalid User Id");
+          else showError("Invalid Password" );
+        } else {
+          if (data.Role === "ADST") {
+            navigate("/" + data.Name, { state: { DD: data.Role, user: data.Name, id: data.Id, Account: data.Account } });
+          } else if (data.Role === "DDST") {
+            navigate("/" + data.Name, { state: { DD: data.Role, user: data.Name, id: data.Id, Account: data.Account } });
+          } else if (data.Role === "DGST") {
+            navigate("/" + data.Name, { state: { DD: data.Role, user: data.Name, id: data.Id, Account: data.Account } });
+          } else if(data.Role === "MANUFACTURER") {
+            navigate("/" + data.Name, { state: { DD: data.Role, user: data.Name, id: data.Id, Account: data.Account } });
+          }
+        }
+      }
+    } catch (err) {
+      showError("An error occurred");
+=======
+import {toast} from "react-toastify";
+export default function Log(){
+const navigate=useNavigate();
+    const [values,setvalues]=useState({Id:"",Password:""}) 
+    const generateError=(err)=>toast.error(err,{
+        position:"bottom-right",
+    });
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        try{
+            const {data} =await axios.get(`https://blockchainscm.onrender.com/Login?param1=${values.Id}&param2=${values.Password}`,{...values},{withCredentials:true});
+            if(data){
+                if(data.errors){
+                    const {email,password}=data.errors;
+                    if(email) generateError(email);
+                    else if(password) generateError(password);
+                }
+                else{
+                    // dispatch({type:"LOGIN_SUCCESS",payload:data.data});
+                    if(data.Role==="ADST"){
+                        navigate("/"+data.Name,{state:{"DD":data.Role,"user":data.Name,"id":data.Id,"Account":data.Account}}) ;
+                    }
+                    else if(data.Role==="DDST"){
+                        navigate("/"+data.Name,{state:{"DD":data.Role,"user":data.Name,"id":data.Id,"Account":data.Account}}) ;
+                    }
+                    else if(data.Role==="DGST"){
+                        navigate("/"+data.Name,{state:{"DD":data.Role,"user":data.Name,"id":data.Id,"Account":data.Account}}) ;
+                    }
+                    else{
+                        navigate("/"+data.Name,{state:{"DD":data.Role,"user":data.Name,"id":data.Id,"Account":data.Account}}) ;
+                    }
+                }
+            }
+        }catch(err){
+        };
+>>>>>>> 7b49bcd6b0d9324c4b9fad12c42cb686fbc9fa54
+    }
+  };
+
+  const showError = (err) => {
+    toast.error(err, {
+      position: "bottom-right",
+    });
+  };
+    return(
+        <section className="vh-100">
+            <div className="container-fluid h-custom">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="col-md-9 col-lg-6 col-xl-5">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                     className="img-fluid" alt="Sample image"/>
+                </div>
+                <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <form  onSubmit={(e)=>handleSubmit(e)}>
+                    {/* Email input */}
+                    <div className="form-outline mb-4">
+
+                    </div>
+                    <div className="form-outline mb-4">
+                    <input type="text" id="user_id" className="form-control form-control-lg"
+                        placeholder="Enter a valid user Id" name="Id" onChange={(e)=>setvalues({...values,[e.target.name]:e.target.value})}/>
+                    <label className="form-label" for="form3Example3">user-id</label>
+                    </div>
+                {/* Password input  */}
+                    <div className="form-outline mb-3">
+                    <input type="password" id="n2" class="form-control form-control-lg"
+                        placeholder="Enter password" name="Password" onChange={(e)=>setvalues({...values,[e.target.name]:e.target.value})}/>
+                    <label className="form-label" for="form3Example4">Password</label>
+                    </div>
+                    <div className="text-center text-lg-start mt-4 pt-2">
+                    <button type="submit" className="btn btn-primary btn-lg">Login</button>
+                        {/* style="padding-left: 2.5rem; padding-right: 2.5rem;"  */}
+                    </div>
+                    <a href="/ResetPassword">Forgot Password</a>
+                <ToastContainer/>
+                </form>
+                
+                </div>
+                
+            </div>
+            </div>
+            <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+
+            <div className="text-white mb-3 mb-md-0">
+                Copyright © 2023. All rights reserved.
+            </div>
+            </div>
+         </section>
+    )
+    
+}
---
+++ my-app/react/src/components/RequestSent.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef, useState } from "react";
+import NavBar from "./NavBar";
+import { useLocation, useNavigate } from "react-router-dom";
+import Web3 from "web3";
+import ABI1 from "../TrufleAbi/ABI1.json";
+import $ from "jquery";
+import "datatables.net";
+import "datatables.net-dt/css/jquery.dataTables.css";
+
+export default function Request() {
+  let account;
+  const navigate = useNavigate();
+  const [res, setRes] = useState([]);
+  const [isRadio,setIsRadio]=useState(1);
+  const tableRef = useRef();
+  const { state } = useLocation();
+  let x = state.user;
+
+  let k=1
+  const HandleSubmit=async(e)=>{
+    setIsRadio(e.target.value);
+    k=e.target.value
+    get();
+}
+  const get = async () => {
+    setRes([])
+      let account;
+      const ABI = ABI1;
+      const Address = "0xFFd25E691AF85B458ac687F35c1918C9EA1ec46C";
+      const { ethereum } = window;
+      window.web3 = await new Web3(ethereum);
+      if (window.ethereum !== "undefined") {
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+        account = accounts[0];
+      }
+      window.web3 = await new Web3(window.ethereum);
+      window.contract = await new window.web3.eth.Contract(ABI, Address);
+      const data = await window.contract.methods.getMyStructs().call();
+      for (let i = 0; i < data.length; i++) {
+        if(k==1){
+          if (x == data[i][0]) {
+            if (!res.includes(data[[i]])) {
+              setRes((prev) => [...prev, data[i]]);
+            }
+          }
+        }
+        else{
+          if (x == data[i][4] && data[i][5]=="Approved") {
+            if (!res.includes(data[[i]])) {
+              setRes((prev) => [...prev, data[i]]);
+            }
+          }
+        }
+      }
+    };
+  useEffect(() => {
+    get();
+    
+  }, []);
+  useEffect(() => {
+    if (res.length > 0) {
+      $(tableRef.current).DataTable();
+    }
+  }, [res]);
+
+  return (
+    <>
+      <div className="container">
+      <input type="radio" id="m1" name="fav_language" value={1} onChange={(e)=>HandleSubmit(e)}/>
+                <label for="m1" style={{marginRight:"20px"}}> My Requests</label>
+                <input type="radio" id="m2" name="fav_language" value={2} onChange={(e)=>HandleSubmit(e)}/>
+                <label for="m2">Requests Approved By Me</label><br/>
+        {isRadio==1?
+        
+          <h6>My Requests</h6>:
+          <h6>Requests Approved By Me</h6>
+        }
+        {res.length==0?
+            <div class="row align-items-center g-lg-5 py-5">
+                        <div class="col-lg-7 text-center text-lg-start">
+                        <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">PENDING REQUESTS NOT AVAILABLE</h1>
+                        <p class="col-lg-10 fs-4"></p>
+                        </div>
+                        <div class="col-md-10 mx-auto col-lg-5">
+                        <div class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+                        <p> WAITING FOR YOUR COMMAND MY BROTHER</p>
+                                <img src="https://cdn.dribbble.com/users/4161631/screenshots/9511726/media/63ad021948f35bf9f89471e50c8b0610.jpg?compress=1&resize=1200x900&vertical=center" class="img-fluid " alt="..."/>
+                            </div>
+                            </div>
+                    </div>
+            :
+        <table className="table table-striped"   ref={tableRef}>
+          <thead>
+            <tr>
+              <th>Request Id</th>
+              <th>Product Name</th>
+              <th>Quantity</th>
+              <th>Date</th>
+              <th>Approved by</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody id="tbody">
+            {res.map((data) => (
+              <tr key={data[6]}>
+                <td>{data[0]} </td>
+                <td>{data[1]} </td>
+                <td>{data[2]}</td>
+                <td>{data[3]}</td>
+                <td>{data[4]}</td>
+                {data[5]==="Approved"?
+                <td>{data[7]}</td>:
+                <td>{data[5]}</td>}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        }
+      </div>
+   </>
+  );
+}
