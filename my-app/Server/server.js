630a9c891f8389466c9f91bd97ae1ce2f6bbca32
SaiKiran474 <130563472+SaiKiran474@users.noreply.github.com>
2023-07-17 13:16:13 +0530

    Update server.js

--- my-app/Server/server.js
+++ my-app/Server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin',8000);
+  res.setHeader('Access-Control-Allow-Origin',BASE_URL1);
   // res.setHeader('Access-Control-Allow-Origin', 'http://192.168.194.213:3000');
   
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
