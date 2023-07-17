97c1a702ae2d8fb7125fe58f73ab8b8af9ea8971
SaiKiran474 <130563472+SaiKiran474@users.noreply.github.com>
2023-07-17 12:24:39 +0530

    Update server.js

--- my-app/Server/server.js
+++ my-app/Server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 // app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://blockchainscm.netlify.app');
   // res.setHeader('Access-Control-Allow-Origin', 'http://192.168.194.213:3000');
   
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
