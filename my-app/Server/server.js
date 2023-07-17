b36ab7672456f400061432bb53402b7f105c639e
SaiKiran474 <130563472+SaiKiran474@users.noreply.github.com>
2023-07-17 14:32:57 +0530

    Update server.js

--- my-app/Server/server.js
+++ my-app/Server/server.js
@@ -18,12 +18,18 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
   }
-  // res.setHeader('Access-Control-Allow-Origin', 'http://192.168.194.213:3000');
-  
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  // Rest of your CORS middleware configuration
+
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.sendStatus(200);
+    return;
+  }
+
   next();
 });
 
